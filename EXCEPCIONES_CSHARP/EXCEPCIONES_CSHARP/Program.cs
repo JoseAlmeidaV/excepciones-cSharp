a9cae52cc07cc8307895c6bce5bf671733cc08a9
JoseAlmeidaV <105743123+JoseAlmeidaV@users.noreply.github.com>
2022-05-19 11:44:45 -0700

    v2
    practica en clase

--- EXCEPCIONES_CSHARP/EXCEPCIONES_CSHARP/Program.cs
+++ EXCEPCIONES_CSHARP/EXCEPCIONES_CSHARP/Program.cs
@@ -10,6 +10,27 @@ namespace EXCEPCIONES_CSHARP
     {
         static void Main(string[] args)
         {
+
+            try
+            {
+                Console.Write("Coloque un numero: ");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.Write("Coloque el segundo numero: ");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(num1 / num2);
+                Console.ReadKey();
+            }catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+               
+            }
         }
     }
 }
